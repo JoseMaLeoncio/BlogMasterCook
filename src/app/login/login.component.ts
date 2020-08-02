59942457ece52aa496ba4da34a6cb7725bc74059
ArianaYaneliLO <66385722+ArianaYaneliLO@users.noreply.github.com>
2020-08-02 15:55:40 -0500

    Login funcional

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AuthserviceService } from '../services/authservice.service';
 @Component({
   selector: 'app-login',
@@ -6,9 +6,10 @@ import { AuthserviceService } from '../services/authservice.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  @Input() deviceXs: boolean;
+  topVal = 0;
   constructor(public auth:AuthserviceService) {
-
+    
   }
   errorMessage:string;
 
@@ -20,6 +21,19 @@ export class LoginComponent implements OnInit {
     console.log($event.value);
     this.errorMessage=this.auth.login($event.value.email,$event.value.password);
     
+  }
+
+  onScroll(e) {
+    let scrollXs = this.deviceXs ? 55 : 73;
+    if (e.srcElement.scrollTop < scrollXs) {
+      this.topVal = e.srcElement.scrollTop;
+    } else {
+      this.topVal = scrollXs;
+    }
+  }
+  sideBarScroll() {
+    let e = this.deviceXs ? 160 : 130;
+    return e - this.topVal;
   }
 
 }
